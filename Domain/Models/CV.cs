5d4a19ae1f19fe9a56ac9ad00cba77a9850061d1
Ackerblad <johanackerblad@outlook.com>
2025-01-07 14:10:58 +0100

    Created models

---
+++ Domain/Models/CV.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class CV
+    {
+        public Guid Id { get; set; }
+        public string FileUrl { get; set; } = string.Empty;
+        public DateTime UploadDate { get; set; }
+
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        public List<CV> CVs { get; set; } = new List<CV>();
+        public List<JobbApplication> JobbApplications { get; set; } = new List<JobbApplication>();
+    }
+}
