1cec7cfadc8185fc0077388b077e5e8197cc7cb4
MalinJonasson <147442348+MalinJonasson@users.noreply.github.com>
2025-01-08 12:04:14 +0100

    Added database context with Identity support and DbSet properties for CV and job applications.

--- Infrastructure/Databases/Database.cs
+++ Infrastructure/Databases/Database.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Databases
 {
-    public class Database
+    public class Database : IdentityDbContext
     {
+        public Database(DbContextOptions<Database> options) : base(options) { }
+
+        public DbSet<CV> CVs { get; set; }
+        public DbSet<JobbApplication> JobbApplications { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SqlDatabase;Trusted_Connection=True;TrustServerCertificate=true;");
+            }
+        }
     }
+
 }
