6c8aa1f03f28f132f42e8f579837f937b6d6fce3
TobiasGoransson <tobiasgoransson1@gmail.com>
2025-01-08 10:35:18 +0100

    Setup Generic Repository

---
+++ Application/Interfaces/RepoInterface/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.RepoInterface
+{
+    public interface IRepository<T>
+    {
+        Task<T> CreateAsync(T entity);
+
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+        Task<List<T>> GetAllAsync();
+
+        Task<string> DeleteByIdAsync(int id);
+
+        Task UpdateAsync(T entity, CancellationToken cancellationToken);
+    }
+}
