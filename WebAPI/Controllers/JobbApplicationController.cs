b5b1c2905f7e2c4a1a0a715ccbb51eb54448a6cf
MalinJonasson <147442348+MalinJonasson@users.noreply.github.com>
2025-01-15 10:47:17 +0100

    upgraded packages and removec APISite

--- WebAPI/Controllers/JobbApplicationController.cs
+++ WebAPI/Controllers/JobbApplicationController.cs
@@ -1,6 +1,6 @@
 ﻿using Application.Interfaces.RepoInterface;
 using Domain.Models;
-using Infrastructure.Repository;
+//using Infrastructure.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
