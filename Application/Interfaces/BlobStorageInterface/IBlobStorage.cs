8f1906ae4e3724dab00cc50ec415b0c2122c0b72
Ackerblad <johanackerblad@outlook.com>
2025-01-20 17:23:00 +0100

    Added Delete and Update functions to BlobStorageService

--- Application/Interfaces/BlobStorageInterface/IBlobStorage.cs
+++ Application/Interfaces/BlobStorageInterface/IBlobStorage.cs
@@ -6,5 +6,7 @@ namespace Application.Interfaces.BlobStorageInterface
     {
         Task<string> UploadFileAsync(IFormFile file);
         Task<Stream> DownloadFileAsync(string blobName);
+        Task<string> UpdateFileAsync(string oldBlobName, IFormFile newFile);
+        Task<bool> DeleteFileAsync(string blobName);
     }
 }
--- Infrastructure/Services/BlobStorageService/BlobStorageService.cs
+++ Infrastructure/Services/BlobStorageService/BlobStorageService.cs
@@ -52,5 +52,32 @@ namespace Infrastructure.Services.BlobStorageService
             var result = await blobClient.DownloadAsync();
             return result.Value.Content;
         }
+
+        public async Task<string> UpdateFileAsync(string oldBlobName, IFormFile newFile)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+
+            var oldBlobClient = containerClient.GetBlobClient(oldBlobName);
+            await oldBlobClient.DeleteIfExistsAsync();
+
+            var newBlobClient = containerClient.GetBlobClient(newFile.FileName);
+
+            using (var stream = newFile.OpenReadStream())
+            {
+                await newBlobClient.UploadAsync(stream, true);
+            }
+
+            return newBlobClient.Uri.ToString();
+        }
+
+        public async Task<bool> DeleteFileAsync(string blobName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            var response = await blobClient.DeleteIfExistsAsync();
+
+            return response;
+        }
     }
 }
