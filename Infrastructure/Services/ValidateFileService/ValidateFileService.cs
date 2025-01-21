60644f782d18690a55aa9b8fae6744d4f1863231
Ackerblad <johanackerblad@outlook.com>
2025-01-21 17:26:23 +0100

    .

---
+++ Infrastructure/Services/ValidateFileService/ValidateFileService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces.ValidateFileInterface;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Services.ValidateFileService
+{
+    public class ValidateFileService : IValidateFile
+    {
+        public async Task<bool> IsValidPDFAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
